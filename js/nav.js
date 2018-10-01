45212f5ff8f7ab497198a81ad084adae08c48585
Selina <31773266+selinagerosa@users.noreply.github.com>
2018-10-01 11:18:16 -0400

    adds new secondary nav, added to home and minister home page

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+(function($) {
+  var endposition = $('.parent').height()-300;
+  var startposition = $('header').height()+300;
+    $(document).ready(function(){
+        $(window).scroll(function(){
+          if($(window).width() >= 500){
+            if ($(this).scrollTop() > endposition) {
+              $('#nav2').fadeOut(500);
+            }
+            else if ($(this).scrollTop() > startposition) {
+                $('#nav2').fadeIn(500);
+            } else {
+                $('#nav2').fadeOut(500);
+            }
+          }
+        });
+    });
+})(jQuery);
