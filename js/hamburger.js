5f2b156aa734f302c60b998346a0ffb6b7298f7d
Selina <31773266+selinagerosa@users.noreply.github.com>
2019-01-31 19:05:40 -0500

    removed calendar page, adds calendar to sp. services, adds services table, update nav, adds toggle to nav 2

--- js/hamburger.js
+++ js/hamburger.js
@@ -11,8 +11,12 @@ function expand() {
   var nav2 = document.getElementById('nav2');
   if (nav2.style.display=='' || nav2.style.display=='none') {
     nav2.style.display='flex';
+    document.getElementById('minus').style.display="inline";
+    document.getElementById('plus').style.display="none";
   }
   else {
     nav2.style.display='none';
+    document.getElementById('minus').style.display="none";
+    document.getElementById('plus').style.display="inline";  
   }
 }
