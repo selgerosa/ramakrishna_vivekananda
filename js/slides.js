a55e8b6877a9fe86f2dde65dffb6b9090f2b88e4
Selina <31773266+selinagerosa@users.noreply.github.com>
2019-11-13 13:37:38 -0500

    adds 3rd button to home, starts slideshow automation

--- js/slides.js
+++ js/slides.js
@@ -5,9 +5,11 @@ function currentSlide(index,slides) {
   showSlides(slideIndex=index, slides);
 }
 function plusSlide(index,slides) {
+  displayon(index+1);
   showSlides(slideIndex=index+1, slides);
 }
 function minusSlide(index,slides) {
+  displayon(index-1);
   showSlides(slideIndex=index-1,slides);
 }
 function showSlides(index,slides) {
@@ -27,17 +29,38 @@ function changeNav(inid, outid) {
 }
 
 function displayon(inid) {
-  for (i=1; i<3; i++) {
+  var len = document.getElementsByClassName("homeslidedesc").length + 1;
+  if (inid >= len) {inid = 1}
+  if (inid < 1) {inid = len-1}
+  for (i=1; i<len; i++) {
     document.getElementById('home'+i).style.display="none";
   }
-  document.getElementById(inid).style.display="flex";
+  document.getElementById('home'+inid).style.display="flex";
+  currentSlide(inid,25);
 }
 
 function displayoff(inid) {
-  document.getElementById(inid).style.display="none";
-}
-function homeoff() {
-  for (i=1; i<3; i++) {
+  var len = document.getElementsByClassName("homeslidedesc").length + 1;
+  if (inid >= len) {inid = 1}
+  if (inid < 1) {inid = len-1}
+  for (i=1; i<len; i++) {
     document.getElementById('home'+i).style.display="none";
   }
+  document.getElementById('home'+inid).style.display="none";
+}
+function homeoff() {
+  var len = document.getElementsByClassName("homeslidedesc").length + 1;
+  for (i=1; i<len; i++) {
+    document.getElementById('home'+i).style.display="none";
+  }
+}
+function automate() {
+  var i =1;
+  currentSlide(i,25);
+  i++;
+  var len = document.getElementsByClassName("homeslidedesc").length + 1;
+  if (i >= len) {i = 1}
+  if (i < 1) {i = len-1}
+  console.log(slideIndex);
+  setInterval(automate, 1000);
 }
