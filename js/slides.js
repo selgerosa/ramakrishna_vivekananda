aee02dc922b9749907608debf44194ef7d494178
Selina <31773266+selinagerosa@users.noreply.github.com>
2019-11-14 13:37:42 -0500

    turns off background/div linking, turns on automatic cycle

--- js/slides.js
+++ js/slides.js
@@ -6,11 +6,11 @@ function currentSlide(index,slides) {
 }
 function plusSlide(index,slides) {
   displayon(index+1);
-  showSlides(slideIndex=index+1, slides);
+  //showSlides(slideIndex=index+1, slides);
 }
 function minusSlide(index,slides) {
   displayon(index-1);
-  showSlides(slideIndex=index-1,slides);
+  //showSlides(slideIndex=index-1,slides);
 }
 function showSlides(index,slides) {
   var i;
@@ -36,7 +36,7 @@ function displayon(inid) {
     document.getElementById('home'+i).style.display="none";
   }
   document.getElementById('home'+inid).style.display="flex";
-  currentSlide(inid,25);
+  //currentSlide(inid,25);
 }
 
 function displayoff(inid) {
@@ -53,14 +53,4 @@ function homeoff() {
   for (i=1; i<len; i++) {
     document.getElementById('home'+i).style.display="none";
   }
-}
-function automate() {
-  var i =1;
-  currentSlide(i,25);
-  i++;
-  var len = document.getElementsByClassName("homeslidedesc").length + 1;
-  if (i >= len) {i = 1}
-  if (i < 1) {i = len-1}
-  console.log(slideIndex);
-  setInterval(automate, 1000);
 }
